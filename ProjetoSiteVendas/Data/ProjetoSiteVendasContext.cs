f37e20d32afad4fff4349917369bad5193e79283
Eros Eloi <eroseloi873@gmail.com>
2023-05-01 12:41:52 -0300

    Inicio do projeto

---
+++ ProjetoSiteVendas/Data/ProjetoSiteVendasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSiteVendas.Models;
+
+namespace ProjetoSiteVendas.Data
+{
+    public class ProjetoSiteVendasContext : DbContext
+    {
+        public ProjetoSiteVendasContext (DbContextOptions<ProjetoSiteVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoSiteVendas.Models.Department> Department { get; set; } = default!;
+    }
+}
---
+++ ProjetoSiteVendas/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoSiteVendas.Models
+{
+    [Table("Department")]
+    public class Department
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Column("Name")]
+        public string Name { get; set; }
+
+    }
+}
